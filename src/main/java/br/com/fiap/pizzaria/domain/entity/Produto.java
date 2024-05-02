41bea2e5702bd42a94f5b1483d5360a3af8b8b3f
Benefrancis <benefrancis@gmail.com>
2024-05-02 11:59:44 -0300

    Parte I da Correção do CP 2

--- src/main/java/br/com/fiap/pizzaria/domain/entity/Produto.java
+++ src/main/java/br/com/fiap/pizzaria/domain/entity/Produto.java
@@ -1,5 +1,6 @@
 package br.com.fiap.pizzaria.domain.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,58 @@ import java.util.Set;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(
+        name = "TB_PRODUTO",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "UK_SABOR_PRODUTO",
+                        columnNames = {"NM_PRODUTO", "SABOR"}
+                )
+        }
+)
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PRODUTO")
+    @SequenceGenerator(name = "SQ_PRODUTO", sequenceName = "SQ_PRODUTO", allocationSize = 1)
+    @Column(name = "ID_PRODUTO")
     private Long id;
+
+
+    @Column(name = "NM_PRODUTO", nullable = false, length = 100)
     private String nome;
+
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "SABOR",
+            referencedColumnName = "ID_SABOR",
+            foreignKey = @ForeignKey(name = "FK_PRODUTO_SABOR")
+    )
     private Sabor sabor;
+
+
     private BigDecimal preco;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "TB_PRODUTO_OPCIONAL",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "PRODUTO",
+                            referencedColumnName = "ID_PRODUTO",
+                            foreignKey = @ForeignKey(name = "FK_PRODUTO_OPCIONAL")
+                    )
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "OPCIONAL",
+                            referencedColumnName = "ID_OPCIONAL",
+                            foreignKey = @ForeignKey(name = "FK_OPCIONAL_PRODUTO")
+                    )
+            }
+    )
     private Set<Opcional> opcionais = new LinkedHashSet<>();
 }
--- src/main/java/br/com/fiap/pizzaria/domain/entity/Sabor.java
+++ src/main/java/br/com/fiap/pizzaria/domain/entity/Sabor.java
@@ -1,5 +1,6 @@
 package br.com.fiap.pizzaria.domain.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(
+        name = "TB_SABOR",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_SABOR_NOME", columnNames = "NM_SABOR")
+        }
+)
+
 public class Sabor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_SABOR")
+    @SequenceGenerator(name = "SQ_SABOR", sequenceName = "SQ_SABOR", allocationSize = 1)
+    @Column(name = "ID_SABOR")
     private Long id;
+
+    @Column(name = "NM_SABOR", nullable = false, length = 100)
     private String nome;
+
+    @Column(name = "DS_SABOR", length = 255)
     private String descricao;
 }
